d0c090d8f42c00e2a48ab48c1552779fd5c1ca46
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2015-03-29 14:43:32 +0200

    initial commits

---
+++ main.py
@@ -0,0 +1,5 @@
+import network
+import mnist_loader
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = network.Network([784, 30, 10])
+net.stochastic_gradient_descent(training_data, 30, 10, 3.0, test_data=test_data)
