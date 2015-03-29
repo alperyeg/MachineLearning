ca65b27833e980305415d7613805f3849799385e
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2015-03-29 15:07:44 +0200

    removed files

--- main.py
+++
@@ -1,5 +0,0 @@
-import network
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.stochastic_gradient_descent(training_data, 30, 10, 3.0, test_data=test_data)
