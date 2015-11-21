8465639826b29d772c5aaa40acc3e2edc5771965
alperyeg <iL2U*f@XJ%V7>
2015-11-21 17:49:33 +0100

    added perceptron class

---
+++ perceptron.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class Perceptron(object):
+    """docstring for Perceptron"""
+
+    def __init__(self, eta=0.01, epochs=50):
+        self.eta = eta
+        self.epochs = epochs
+
+    def train(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.epochs):
+            errors = 0
+            for xi, target in zip(X, y):
+                # delta_w = eta(target_i-output_i)x_i
+                update = self.eta * (target - self.predict(xi))
+                # wTx
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                # update errors
+                errors += int(update != 0.0)
+            # error per epoch
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:] + self.w_[0])
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+# choose setose and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+
+import matplotlib.pyplot as plt
+from decision_regions import plot_decision_regions
+
+ppn = Perceptron(epochs=10, eta=0.1)
+ppn.train(X, y)
+print('Weights: %s' % ppn.w_)
+plot_decision_regions(X, y, clf=ppn)
+plt.title('Perceptron')
+plt.xlabel('sepal length in cm')
+plt.ylabel('petal length in cm')
+plt.show()
+
+plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
+plt.xlabel('Iterations')
+plt.ylabel('Missclassifications')
+plt.show()
