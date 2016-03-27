dddce9d64ce0f90af27eee4be74c21c5f53619d8
alperyeg <a.yegenoglu@fz-juelich.de>
2016-03-27 19:22:05 +0200

    added AdalineSGD

---
+++ AdalineSGD.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+class AdalineSGD(object):
+    """
+    Adaptive Linear Neuron Stochastic Gradient Descent
+    """
+    def __init__(self):
+        pass
