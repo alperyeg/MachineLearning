136f43f0e6189787e9170825f8bd347dbbc07495
alperyeg <a.yegenoglu@fz-juelich.de>
2016-10-02 19:36:32 +0200

    added example plot for sigmoidal function

---
+++ sigmoidal.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.axhspan(0.0, 1.0, facecolor='1.0', alpha=1.0, ls='dotted')
+plt.axhline(y=0.5, ls='dotted', color='k')
+plt.yticks([0.0, 0.5, 1.0])
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+plt.show()
