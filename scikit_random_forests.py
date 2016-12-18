cbed51e0eaf7c8633987db405836d98d934c6b2e
alperyeg <a.yegenoglu@fz-juelich.de>
2016-12-18 18:06:54 +0100

    added random forest and updated range in decision tree

--- scikit_random_forests.py
+++ scikit_random_forests.py
@@ -1,6 +1,6 @@
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.ensemble import RandomForestClassifier
 from decision_regions import plot_decision_regions_
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,17 +15,15 @@ y = iris.target
 # Split X and y in into 30 % test data and 70 % into training data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
                                                     random_state=0)
-tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
-tree.fit(X_train, y_train)
+forest = RandomForestClassifier(criterion='entropy', n_estimators=10,
+                                random_state=1, n_jobs=4)
+forest.fit(X_train, y_train)
 X_combined = np.vstack((X_train, X_test))
 y_combined = np.hstack((y_train, y_test))
 
-plot_decision_regions_(X_combined, y_combined, classifier=tree,
+plot_decision_regions_(X_combined, y_combined, classifier=forest,
                        test_idx=range(105, 150))
 plt.xlabel('petal length (cm)')
 plt.ylabel('petal width(cm)')
 plt.legend(loc='upper left')
 plt.show()
-
-export_graphviz(tree, out_file='tree.dot',
-                feature_names=['petal length', 'petal width'])
