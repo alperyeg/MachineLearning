81378c48b3b8e50913171409791d5f4e89e80eec
alperyeg <a.yegenoglu@fz-juelich.de>
2017-04-19 19:26:19 +0200

    added discriminator

--- GAN/gan_tutorial1.py
+++ GAN/gan_tutorial1.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm
 
+# Set seed, for reproducibility
+tf.set_random_seed(1234)
+
 # Target distribution p_data
 mu, sigma = -1, 1
 xs = np.linspace(-5, 5, 1000)
@@ -24,6 +27,8 @@ M = 200
 
 # MLP - for D_pre, D1, D2, G networks
 def mlp(inpt, output_dim):
+    # tf.get_variable if you want to share variables, otherwise tf.Variable
+    # is also fine
     w1 = tf.get_variable('w0', [inpt.get_shape()[1], 6],
                          initializer=tf.random_normal_initializer())
     b1 = tf.get_variable('b0', [6], initializer=tf.constant_initializer(0.0))
@@ -39,3 +44,74 @@ def mlp(inpt, output_dim):
     fc2 = tf.nn.tanh(tf.matmul(fc1, w2) + b2)
     fc3 = tf.nn.tanh(tf.matmul(fc2, w3) + b3)
     return fc3, [w1, b1, w2, b2, w3, b3]
+
+
+# optimize networks
+def momentum_optimizer(loss, var_list):
+    batch = tf.Variable(0)
+    # Apply exponential decay to the learning rate, staircase to use integer
+    #  division in a stepwise (=staircase) fashion
+    learning_rate = tf.train.exponential_decay(0.001, batch, TRAIN_ITERS //
+                                               4, 0.95, staircase=True)
+    # optimizer=tf.train.GradientDescentOptimizer(learning_rate).minimize(
+    #     loss, global_step=batch,var_list=var_list)
+    optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate,
+                                           momentum=0.6).minimize(loss,
+                                                                  global_step=batch,
+                                                                  var_list=var_list)
+    return optimizer
+
+# pre train decision surface
+with tf.variable_scope("D_pre"):
+    input_node = tf.placeholder(tf.float32, shape=(M, 1))
+    train_labels = tf.placeholder(tf.float32, shape=(M, 1))
+    D, theta = mlp(input_node, 1)
+    loss = tf.reduce_mean(tf.square(D - train_labels))
+    optimizer = momentum_optimizer(loss, None)
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+
+# Plot the decision surface
+def plot_decision_surface(D, inpt_node):
+    f, ax = plt.subplots(1)
+    # p_data
+    xs = np.linspace(-5, 5, 1000)
+    ax.plot(xs, norm.pdf(xs, loc=mu, scale=sigma), label='p_data')
+    # decision boundary
+    r = 1000    # resolution = #points
+    xs = np.linspace(-5,5, r)
+    # decision surface
+    ds = np.zeros((r, 1))
+    for i in range(r/M):
+        x = np.reshape(xs[M*i:M*(i+1)],(M,1))
+        ds[M * i:M * (i + 1)] = sess.run(D, {inpt_node: x})
+
+    ax.plot(xs, ds, label='decision boundary')
+    ax.set_ylim(0, 1.1)
+    plt.legend()
+    plt.title('Initial Decision Boundary')
+    plt.show()
+
+
+plot_decision_surface(D, input_node)
+
+lh = np.zeros(1000)
+for i in range(1000):
+    # d=np.random.normal(mu,sigma,M)
+    d = (np.random.random(M) - 0.5) * 10.0
+    labels = norm.pdf(d, loc=mu, scale=sigma)
+    lh[i], _ = sess.run([loss, optimizer], {input_node: np.reshape(d, (M, 1)),
+                                            train_labels: np.reshape(labels,
+                                                                     (M, 1))})
+# training loss
+plt.plot(lh)
+plt.title('Training Loss')
+plot_decision_surface(D,input_node)
+# copy the learned weights over into a tmp array
+weightsD = sess.run(theta)
+# close the pre-training session
+sess.close()
+plt.show()
+
+# TODO: Building the generative net
