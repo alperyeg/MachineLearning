b8bdcb1fc5ee56ac6b46ca398bedec7738891f0e
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2017-11-10 16:10:56 +0100

    fixed spelling mistake

--- scikit_kernelsvm_xor.py
+++ scikit_kernelsvm_xor.py
@@ -8,7 +8,7 @@ X_xor = np.random.randn(200, 2)
 y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
 y_xor = np.where(y_xor, 1, -1)
 
-svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=1.0)
 svm.fit(X_xor, y_xor)
 plot_decision_regions_(X_xor, y_xor, classifier=svm)
 plt.legend(loc='upper left')
@@ -19,7 +19,7 @@ def plot_xor(X_xor, y_xor):
     plt.scatter(X_xor[y_xor == 1, 0], X_xor[y_xor == 1, 1],
                 c='b', marker='x', label='1')
     plt.scatter(X_xor[y_xor == -1, 0], X_xor[y_xor == -1, 1],
-                c='r', arker='s', label='-1')
+                c='r', marker='s', label='-1')
     plt.ylim(-3.0)
     plt.legend()
     plt.show()
